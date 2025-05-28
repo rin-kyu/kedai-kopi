1d12444d82942ce556266a17cced1009538667e4
rin-kyu <septianrino883@gmail.com>
2025-05-28 08:56:25 +0700

    merubah detail pada produk

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('Product', () => ({
         items: [
-            {id: 1, name: 'Robusta Brazil', img: '1.jpg', price: 20000},
-            {id: 2, name: 'Arabica Blend', img: '2.jpg', price: 30000},
-            {id: 3, name: 'Primo Passo', img: '3.jpg', price: 25000},
-            {id: 4, name: 'Aceh Gayo', img: '4.jpg', price: 32000},
-            {id: 5, name: 'Sumatra Mantheling', img: '5.jpg', price: 40000},
+            {id: 1, name: 'Robusta Brazil', img: '1.png', price: 20000},
+            {id: 2, name: 'Arabica Blend', img: '2.png', price: 30000},
+            {id: 3, name: 'Primo Passo', img: '3.png', price: 25000},
+            {id: 4, name: 'Aceh Gayo', img: '4.png', price: 32000},
+            {id: 5, name: 'Sumatra Mantheling', img: '5.png', price: 40000},
         ],
     }));
 });
